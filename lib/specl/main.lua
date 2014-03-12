a7d6991c82397ef998b7178013aaf24d1a4f89d2
Gary V. Vaughan <gary@gnu.org>
2014-03-12 21:56:04 +1300

    refactor: refill optspec at 78 columns.
    * lib/specl/main.lua (optspec): Refill at 78 columns.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/specl/main.lua
+++ lib/specl/main.lua
@@ -46,12 +46,12 @@ Usage: specl [OPTION]... [FILE]...
 
 Behaviour Driven Development for Lua.
 
-Develop and run BDD specs written in Lua for RSpec style workflow, by
-verifying specification expectations read from given FILEs or standard
-input, and reporting the results on standard output.
+Develop and run BDD specs written in Lua for RSpec style workflow, by verifying
+specification expectations read from given FILEs or standard input, and
+reporting the results on standard output.
 
-If no FILE is listed, then run specifications for all files from the
-'specs/' directory with names ending in '.yaml'.
+If no FILE is listed, then run specifications for all files from the 'specs/'
+directory with names ending in '.yaml'.
 
 Where '-' is given as a FILE, then read from standard input.
 
@@ -69,29 +69,28 @@ Filtering:
   +NN                    check only the example at line NN in next FILE
   FILE:NN[:MM]           check only the example at line NN in this FILE
 
-When filtering by PATTERN, an example is considered a match if PATTERN
-matches the concatenation of nested YAML descriptions leading directly
-to that example in its spec file.
+When filtering by PATTERN, an example is considered a match if PATTERN matches
+the concatenation of nested YAML descriptions leading directly to that example
+in its spec file.
 
-You can specify +NN (where NN is a line number) multiple times for the
-next specified FILE, or interspersed to specify different line filters
-for different FILEs. Specifying any number of +NN will prevent all
-specifications in that file except those selected by a +NN filter from
-being checked. If +NN is not the first line of an example (as would be
-displayed by a failing example in verbose mode), the option selects no
-examples.
+You can specify +NN (where NN is a line number) multiple times for the next
+specified FILE, or interspersed to specify different line filters for different
+FILEs. Specifying any number of +NN will prevent all specifications in that
+file except those selected by a +NN filter from being checked. If +NN is not
+the first line of an example (as would be displayed by a failing example in
+verbose mode), the option selects no examples.
 
-The alternative FILE:NN:MM syntax makes it easy to cut and paste from
-Specl failure output, but allows only a single line NN to be filtered
-(except when combined with +NN filters).  The optional :MM suffix is
-ignored -- and merely represents the ordinal number of an `expect`
-statement in a particular example in verbose Specl formatter outputs.
+The alternative FILE:NN:MM syntax makes it easy to cut and paste from Specl
+failure output, but allows only a single line NN to be filtered (except when
+combined with +NN filters).  The optional :MM suffix is ignored -- and merely
+represents the ordinal number of an `expect` statement in a particular example
+in verbose Specl formatter outputs.
 
-Due to a shortcoming in libYAML, unicode characters in any passed FILE
-prevent Specl from working. The '--unicode' option works around that
-shortcoming, but any error messages caused by Lua code in FILE will
-usually report line-numbers incorrectly.  By default, errors report
-accurate line-numbers, and unicode characters are not supported.
+Due to a shortcoming in libYAML, unicode characters in any passed FILE prevent
+Specl from working. The '--unicode' option works around that shortcoming, but
+any error messages caused by Lua code in FILE will usually report line-numbers
+incorrectly.  By default, errors report accurate line-numbers, and unicode
+characters are not supported.
 
 Report bugs to @PACKAGE_BUGREPORT@.]]
 
