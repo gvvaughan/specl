2ec8f7d10860d48ef8c2196e68d2b4b49d80b306
Gary V. Vaughan <gary@gnu.org>
2016-01-17 15:32:06 +0000

    maint: bump copyright years.
    * bootstrap.conf, configure.ac, lib/specl/badargs.lua,
    lib/specl/color.lua, lib/specl/compat.lua,
    lib/specl/formatter/progress.lua, lib/specl/formatter/report.lua,
    lib/specl/inprocess.lua, lib/specl/loader.lua, lib/specl/main.lua,
    lib/specl/matchers.lua, lib/specl/runner.lua, lib/specl/shell.lua,
    lib/specl/util.lua, lib/specl/version.lua.in, local.mk, specl.in,
    specs/specs.mk: Bump copyright years to include 2016.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/specl/loader.lua
+++ lib/specl/loader.lua
@@ -1,7 +1,7 @@
 -- Load Specl spec-files into native Lua tables.
 -- Written by Gary V. Vaughan, 2013
 --
--- Copyright (c) 2013-2015 Gary V. Vaughan
+-- Copyright (c) 2013-2016 Gary V. Vaughan
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
