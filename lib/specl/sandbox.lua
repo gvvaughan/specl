df4b1a18175bad2f04960de6b4ef5da4471e5068
Gary V. Vaughan <gary@gnu.org>
2016-03-15 20:50:18 +0000

    sandbox: use `load` to load strings, even on Lua 5.1.
    * lib/specl/sandbox.lua (sandbox): Remove loadstring.
    (load): Use a loadstring/load wrapper when host implementation
    has a `load` that cannot handle strings.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/specl/sandbox.lua
+++ lib/specl/sandbox.lua
@@ -33,6 +33,18 @@ local deepcopy = util.deepcopy
 --[[ ====================================== ]]--
 
 
+local load = load
+if not pcall (load, "_=1") then
+  local loadfunction = load
+  load = function (...)
+    if type (...) == "string" then
+      return loadstring (...)
+    end
+    return loadfunction (...)
+  end
+end
+
+
 local sandbox = {
   _VERSION	= _VERSION,
   arg		= clone (arg),
@@ -86,7 +98,6 @@ local sandbox = {
   ipairs	= ipairs,
   load		= load,
   loadfile	= loadfile,
-  loadstring	= loadstring,
   math = {
     abs		= math.abs,
     acos	= math.acos,
